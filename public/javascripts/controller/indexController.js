1105da3e0281276e7fb7f6a672242f9c0169378b
Emre Can Hayıt <emrecan.hayit@gmail.com>
2020-08-16 13:16:19 +0300

    user con/dis method

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,11 +1,49 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    indexFactory.connectSocket('http://localhost:3000', {
-        reconnectionAttempts: 5,
-        reconnectionDelay: 1000,
-        reconnection: true
-    }).then((socket) => {
-        console.log('bağlantı başarılı', socket);
-    }).catch((err) => {
-        console.log(err);
-    });
+
+    $scope.messages = [ ];
+
+    $scope.init = () => {
+        const username = prompt('Please enter the username');
+
+        if(username) initSocket(username);
+        else return false;
+    };
+
+    function initSocket(username){
+        const connectionRe = {
+            reconnectionAttempts: 5,
+            reconnectionDelay: 1000,
+            reconnection: true
+        }
+        indexFactory.connectSocket('http://localhost:3000', connectionRe)
+        .then((socket) => {
+            socket.emit('newUser', { username });
+
+            socket.on('newUser', (data) => {
+                const userInfo = 'Katıldı.';
+                const messageData = {
+                    type : 0, // info; 0 -> server info, 1 -> userinfo
+                    username : data.username,
+                    text : userInfo
+                };
+
+                $scope.messages.push(messageData);
+                $scope.$apply();
+            });
+
+            socket.on('disUser', (data) => {
+                const userInfo = 'Ayrıldı.';
+                const messageData = {
+                    type : 0, // info; 0 -> server info, 1 -> userinfo
+                    username : data.username,
+                    text : userInfo
+                };
+
+                $scope.messages.push(messageData);
+                $scope.$apply();
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
 }]);
